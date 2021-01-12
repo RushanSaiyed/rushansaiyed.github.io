407322d0cf1140bba6a910385a953f92148d2a4a
Rushan Saiyed <rushansaiyed5884@gmail.com>
2021-01-12 16:02:51 +0530

    first commit

--- README.md
+++
@@ -1,1 +0,0 @@
-# rushansaiyed.github.io
---
+++ js/main.js
@@ -0,0 +1,13 @@
+$('.count').counterUp({
+  delay: 10,
+  time: 2000
+});
+
+var typed = new Typed('.type', {
+    strings: ['','Designer', 'Programmer'],
+    typeSpeed: 60,
+    backSpeed: 60,
+    loop:true
+  });
+
+  
